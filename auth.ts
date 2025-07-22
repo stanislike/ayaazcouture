1c76d822204c2647126892583e04726cfb8c328c
stanislike <s.joulah@outlook.fr>
2025-07-22 19:55:10 +0200

    session cart id cookies and get item from cart

--- auth.ts
+++ auth.ts
@@ -4,6 +4,8 @@ import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config: NextAuthConfig = {
   pages: {
@@ -83,8 +85,31 @@ export const config: NextAuthConfig = {
             data: { name: token.name },
           });
         }
+      }
+      return token;
+    },
+    authorized({ request, auth }: any) {
+      //Check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
 
-        return token;
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        //Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set newly generated sessionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        return response;
+      } else {
+        return true;
       }
     },
   },
--- lib/actions/cart.action.ts
+++ lib/actions/cart.action.ts
@@ -1,11 +1,78 @@
 "use server";
 
 import { CartItem } from "@/types";
+import { cookies } from "next/headers";
+import { convertToPlainObject, formatError } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { get } from "http";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    data,
-    success: true,
-    message: "Produit ajouté au panier",
-  };
+  try {
+    // Get the session cart ID from cookies
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Session cart ID not found");
+
+    // Get session and user ID
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // get cart
+    const cart = await getMyCart();
+
+    // Parse and validate the item
+    const item = cartItemSchema.parse(data);
+
+    // Find product and database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
+    //Testing
+    console.log({
+      sessionCartId: sessionCartId,
+      userId: userId,
+      "Item requested": item,
+      "Product found": product,
+    });
+
+    return {
+      data,
+      success: true,
+      message: "Produit ajouté au panier",
+    };
+  } catch (error) {
+    return {
+      data,
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  // Get the session cart ID from cookies
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Session cart ID not found");
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  //convert decimals and return cart items
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    totalPrice: cart.itemsPrice.toString(),
+    shippingPrice: cart.itemsPrice.toString(),
+    taxPrice: cart.itemsPrice.toString(),
+  });
 }
