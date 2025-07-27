c49a54bdb86aa5e561525da1c9fbc7b084ecdf96
stanislike <s.joulah@outlook.fr>
2025-07-27 22:35:59 +0200

    new style

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -33,7 +33,7 @@ const UserButton = async () => {
           <div className="flex items-center">
             <Button
               variant="ghost"
-              className="relative h-8 w-8 rounded-full ml-2 flex items-center justify-center bg-gray-200"
+              className="relative h-8 w-8 rounded-full ml-2 flex items-center justify-center bg-gray-200 text-[#241e2e]"
             >
               {firstInitial}
             </Button>
