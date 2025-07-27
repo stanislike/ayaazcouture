8fdf759733f67e24396dd26235ae568cdc5c0970
stanislike <s.joulah@outlook.fr>
2025-07-27 23:46:19 +0200

    test vercel

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+export const runtime = "nodejs";
 
 export const config: NextAuthConfig = {
   pages: {
